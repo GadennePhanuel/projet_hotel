172e0af60353809ec322c5ea68c460c08a91ec67
Marie <marievigin@gmail.com>
2020-08-04 18:23:51 +0200

    customer.php

--- VIEW/login.php
+++ VIEW/login.php
@@ -11,7 +11,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>login</title>
 </head>
 <body>
 
@@ -35,7 +35,7 @@
     }
 
     ?>
-
+    <?php unset($_SESSION['displayCustomerRoom']);?>
 </body>
 
 
